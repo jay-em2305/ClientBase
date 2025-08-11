2bc9c467fa8bc0c9c73c4c3e2d7ea64e4a056f88
jay-em2305 <enrique@clientbase.pro>
2025-08-11 09:39:35 +0800

    updated Code

--- send.php
+++ send.php
@@ -2,9 +2,9 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 
 $mail = new PHPMailer(true);
@@ -27,9 +27,9 @@ try {
     ];
 
     $mail->setFrom($_POST['email'], 'Website Mailer');
-    //$mail->addAddress('liliana@clientbase.pro', 'Admin');
-    //$mail->addAddress('denise@clientbase.pro', 'Admin');
-    $mail->addAddress('enrique@clientbase.pro', 'Admin');
+    $mail->addAddress('liliana@clientbase.pro', 'Admin');
+    $mail->addAddress('denise@clientbase.pro', 'Admin');
+    //$mail->addAddress('enrique@clientbase.pro', 'Admin');
 
     $mail->isHTML(true);
     $mail->Subject = 'New Inquiry Signup';
